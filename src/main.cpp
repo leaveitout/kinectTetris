4e8ac2ed0b712e3d6aa47bc5f3a8c2cecb4a9fd4
Seán Bruton <sbruton@tcd.ie>
2014-01-22 14:55:58 +0000

    Added Release display mode.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main()
 {
 	ofAppGlutWindow window; // create a window
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+#ifdef _DEBUG
 	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+#else
+	ofSetupOpenGL(&window, 1024, 768, OF_FULLSCREEN);
+#endif
 	ofRunApp(new testApp()); // start the app
 }
